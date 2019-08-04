933372dfcadd471b6f8ca69d684350044fe2b97b
attentive-rubyist-8517 <robby.medina562@gmail.com>
2019-08-04 06:50:09 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,11 +4,13 @@ return sum
 end
 
 def subtraction(num1, num2)
-
+difference=num2-num1
+return difference
 end
 
 def division(num1, num2)
-
+quotient=num2/num1
+return quotient
 end
 
 def multiplication(num1, num2)
